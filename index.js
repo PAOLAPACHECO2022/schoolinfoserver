74190ccdfe7a392891dae087ff5e774d395c3e12
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-05-24 10:33:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: 'https://schoolinfocliente.onrender.com/' // Cambia esto a la URL de tu frontend desplegado
+  origin: 'https://schoolinfocliente.onrender.com' // Cambia esto a la URL de tu frontend desplegado
 }));
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
